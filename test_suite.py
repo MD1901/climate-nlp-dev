c1816f9f028817a9feea6b1b984ea04c07c2b216
Marius Drozdzewski <marius.drozdzewski@gmail.com>
2020-01-05 11:19:11 +0100

    not fully working yet, a few changes

--- test_suite.py
+++ test_suite.py
@@ -1,20 +1,18 @@
 import polarity_analyser
 
 def test_polarity_without_shifter():
-    model = polarity_analyser.polarity_without_shifter()
-    model("Das hier ist der erste Testsatz")
+    model = polarity_analyser.polarity_without_shifter("Das hier ist der erste Testsatz")
 
 
 def test_polarity_with_shifter():
-    model = polarity_analyser.polarity_with_shifter()
-    model("Das hier ist der erste Testsatz")
+    model = polarity_analyser.polarity_with_shifter("Das hier ist der erste Testsatz")
 
 
 
 
 def text_polarity_with_shifter_other_lexicon():
-    model = polarity_analyser.polarity_with_shifter("lexicon 2")
-    model("Das hier ist der erste Testsatz")
+    model = polarity_analyser.polarity_with_shifter("Das hier ist der erste Testsatz",
+                                                     1)
 
 
 
