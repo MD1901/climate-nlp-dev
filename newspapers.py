afa6b6c4adb22a68d4c47008b0c14762bb766ec1
Marius Drozdzewski <marius.drozdzewski@gmail.com>
2020-03-09 18:24:35 +0100

    new Parsers

--- newspapers.py
+++ newspapers.py
@@ -31,13 +31,27 @@ def check_guardian(link):
         print('rejecting {}'.format(link))
         return False
 
+def check_zeit(link):
+    parts = link.split('/')
+
+    unwanted = ['video']
+    for unw in unwanted:
+        if unw in parts:
+            return False
+
+def check_fox(link):
+    parts = link.split('/')
+
+    unwanted = ['category']
+    for unw in unwanted:
+        if unw in parts:
+            return False
 
 def check(link):
     return link
 
 
 def parse_guardian(link):
-    cls = 'content__article-body from-content-api js-article__body'
     cls = 'content__article-body from-content-api js-article__body'
     html = requests.get(link).text
     soup = BeautifulSoup(html, features="html.parser")
@@ -51,9 +65,71 @@ def parse_guardian(link):
     article = ''.join(article)
     return {'body' :article}
 
+def parse_fox(link):
+    cls = 'article-body'
+    html = requests.get(link).text
+    soup = BeautifulSoup(html, features="html.parser")
+
+    table = soup.findAll('div', attrs={"class": cls})
+    if len(table) != 1:
+        import pdb; pdb.set_trace()
+    assert len(table) == 1
+
+    article = [p.text for p in table[0].findAll('p', attrs={"class": "speakable"})]
+    article = ''.join(article)
+    return {'body' : article}
+
+def parse_newyorker(link):
+    cls = 'grid--item body body__container article__body grid-layout__content'
+    html = requests.get(link).text
+    soup = BeautifulSoup(html, features="html.parser")
+
+    table = soup.findAll('div', attrs={"class": cls})
+    if len(table) != 1:
+        import pdb; pdb.set_trace()
+    assert len(table) == 1
+
+    article = [p.text for p in table[0].findAll('p')]
+    article = ''.join(article)
+    return {'body' : article}
+
+def parse_bild(link):
+    cls = 'txt'
+    html = requests.get(link).text
+    soup = BeautifulSoup(html, features="html.parser")
+
+    table = soup.findAll('div', attrs={"class": cls})
+    if len(table) != 1:
+        import pdb; pdb.set_trace()
+    assert len(table) == 1
+
+    article = [p.text for p in table[0].findAll('p')]
+    article = ''.join(article)
+    return {'body' : article}
+
+def parse_zeit(link):
+    cls = 'article-page'
+    html = requests.get(link).text
+    soup = BeautifulSoup(html, features="html.parser")
+
+    table = soup.findAll('div', attrs={"class": cls})
+    if len(table) != 1:
+        import pdb; pdb.set_trace()
+    assert len(table) == 1
+
+    article = [p.text for p in table[0].findAll('p')]
+    article = ''.join(article)
+    return {'body' : article}
+
 
 newspapers = [
-    {"site": "zeit.de","language": "german", "id": "zeit"},
+    {
+        "site": "zeit.de",
+        "language": "german",
+        "id": "zeit",
+        "checker": check_zeit,
+        "parser": parse_zeit
+    },
     {
         "site": "theguardian.com",
         "language": "english",
@@ -61,19 +137,29 @@ newspapers = [
         "checker": check_guardian,
         "parser": parse_guardian
     },
-    {"site": "bild.de", "language": "german", "id": "bild"},
-    {"site": "newyorker.com","language": "english", "id": "newyorker.com"},
+    {
+        "site": "bild.de",
+        "language": "german",
+        "id": "bild",
+        "parser": parse_bild
+    },
+    {
+        "site": "newyorker.com",
+        "language": "english",
+        "id": "newyorker.com",
+        "parser": parse_newyorker
+    },
     {"site": "nytimes.com", "language": "english", "id": "nytimes"},
     {
         "site": "foxnews.com",
         "language": "english",
         "id": "fox",
-        "checker": check
+        "checker": check_fox,
+        "parser": parse_fox
+
     },
     {"site": "bbc.com", "language": "english", "id": "bbc"},
     {"site": "theaustralian.com.au", "language": "english", "id": "australian"},
     {"site": "news.sky.com/uk", "language": "english", "id": "skyuk"},
     {"site": "skynews.com.au", "language": "english", "id": "skyau"}
 ]
-
-
